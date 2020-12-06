2263e055a1485557c84ef6361c28069791ed6076
David Lineman <dline215@gmail.com>
2020-12-06 11:25:40 -0500

    added a post function for username and password

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res ) => res.render('index'))
 app.listen(3000, () => console.log('Server ready'))
 
+app.post('/', (req, res) => {
+  const { username, password} = req.body
+
+  if ( !usenname || !password ) {
+    res.render('error', {
+      message: 'Please set both username and password'
+    })
+    return
+  }
+  console.log(req.body, username, password)
+  res.end()
+})
